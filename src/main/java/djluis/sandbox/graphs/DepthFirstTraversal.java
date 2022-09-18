c892bdeb5ac20221e90b1c7c79992d6f0ca59610
djluis <dani.jluis@gmail.com>
2022-09-18 01:45:53 +0100

    Implement DepthFirstTraversal and include JGrapthT lib to ease the unit tests and creation of graphs

---
+++ src/main/java/djluis/sandbox/graphs/DepthFirstTraversal.java
@@ -0,0 +1,32 @@
+package djluis.sandbox.graphs;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class DepthFirstTraversal<K> {
+    Set<K> find(Graph<K, DefaultEdge> graph, K root) {
+        Set<K> visited = new LinkedHashSet<>(); // To keep the order
+        Queue<K> queue = new LinkedList<>(); //FIFO
+
+        queue.add(root); // Queue to avoid recursion
+        visited.add(root); // Marked visited to avoid infinite cycles
+
+        while (!queue.isEmpty()) {
+            K vertex = queue.poll();
+            for (DefaultEdge v : graph.edgesOf(vertex)) {
+                K edgeTargetVertex = graph.getEdgeTarget(v);
+                if (!visited.contains(edgeTargetVertex)) {
+                    visited.add(edgeTargetVertex);
+                    queue.add(edgeTargetVertex);
+                }
+            }
+        }
+
+        return visited; // return an ordered set with all the connections to the K root
+    }
+}
