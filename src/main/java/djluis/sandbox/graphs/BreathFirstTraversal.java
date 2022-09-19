ef605badda1afebac58adb54d2ef0b14ae8c15b1
djluis <dani.jluis@gmail.com>
2022-09-19 11:44:21 +0100

    Add Depth first traversal

--- src/main/java/djluis/sandbox/graphs/BreathFirstTraversal.java
+++ src/main/java/djluis/sandbox/graphs/BreathFirstTraversal.java
@@ -4,29 +4,29 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
 
-public class DepthFirstTraversal<K> {
-    public Set<K> find(Graph<K, DefaultEdge> graph, K root) {
+public class BreathFirstTraversal<K> {
+    Set<K> find(Graph<K, DefaultEdge> graph, K root) {
         Set<K> visited = new LinkedHashSet<>(); // To keep the order
-        Stack<K> stack = new Stack<>(); //LIFO
+        Queue<K> queue = new LinkedList<>(); //FIFO
 
-        stack.push(root);  // Stack to avoid recursion
+        queue.add(root); // Queue to avoid recursion
         visited.add(root); // Marked visited to avoid infinite cycles
 
-        while (!stack.isEmpty()) {
-            K vertex = stack.pop();
-
+        while (!queue.isEmpty()) {
+            K vertex = queue.poll();
             for (DefaultEdge v : graph.edgesOf(vertex)) {
                 K edgeTargetVertex = graph.getEdgeTarget(v);
                 if (!visited.contains(edgeTargetVertex)) {
                     visited.add(edgeTargetVertex);
-                    stack.push(edgeTargetVertex);
+                    queue.add(edgeTargetVertex);
                 }
             }
         }
 
-        return visited;
+        return visited; // return an ordered set with all the connections to the K root
     }
 }
