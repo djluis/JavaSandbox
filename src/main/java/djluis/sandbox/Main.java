ff926d10e2cd21daa75e6684a358f956ee922947
djluis <dani.jluis@gmail.com>
2022-09-27 13:00:19 +0100

    shh I need to hide this xD

--- src/main/java/djluis/sandbox/Main.java
+++ src/main/java/djluis/sandbox/Main.java
@@ -2,7 +2,7 @@ package djluis.sandbox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi, I'm Daniel a crazy Software Engineer");
+        System.out.println("Hi, I'm Daniel a Software Engineer");
         System.out.println("Please don't forget to execute the unit tests");
         System.out.println("Bye..........");
     }
