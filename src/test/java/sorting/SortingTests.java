52282b9725aa63d930069d83e0fed00481b60cef
djluis <dani.jluis@gmail.com>
2022-09-30 13:49:46 +0100

    Isolate quickSort for a sorted array scenario

--- src/test/java/sorting/SortingTests.java
+++ src/test/java/sorting/SortingTests.java
@@ -4,19 +4,21 @@ import djluis.sandbox.sorting.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static sorting.SortTestUtils.isArraySorted;
 import static sorting.SortTestUtils.randomArrayGenerator;
 
 
 public class SortingTests {
 
-    private static final int RANDOM_ARRAY_SIZE = 20000;
+    private static final int RANDOM_ARRAY_SIZE = 100000;
 
     private static int[] BASE_RANDOM_ARRAY;
 
@@ -36,15 +38,18 @@ public class SortingTests {
         Assertions.assertTrue(isArraySorted(intArray));
     }
 
-    @DisplayName("Test with @MethodSource")
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("sortAlgorithms")
-    public void testSortingAlgorithmWithSortedArray(SortAlgorithm sortAlgorithm) {
+    @Test
+    public void testSortingAlgorithmWithSortedArray() {
         int[] intArray = new int[BASE_RANDOM_ARRAY.length];
         System.arraycopy(BASE_RANDOM_ARRAY, 0, intArray, 0, intArray.length);
         Arrays.sort(intArray);
-        sortAlgorithm.sort(intArray);
-        Assertions.assertTrue(isArraySorted(intArray));
+
+        // Quicksort with sorted array is O(n^2), that means a huge number of recursion calls
+        QuickSort quickSort = new QuickSort();
+
+        assertThrows(StackOverflowError.class, () ->
+                quickSort.sort(intArray)
+        );
     }
 
     static Stream<SortAlgorithm> sortAlgorithms() {
